41ee396fb39dde748bae615ef83a0b4c19abd601
Dylan Lewis and Max Scher <dylan@>
2017-09-27 13:24:20 -0700

    setup redux

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom"
 import { AppContainer } from "react-hot-loader";
-
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './rootReducer';
 import App from "./components/App";
 
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
   ReactDOM.render(
     <HashRouter>
-      <App/>
+      <Provider store={store}>
+        <App/>
+      </Provider>
     </HashRouter>,
     document.getElementById("react-app-root")
   );
